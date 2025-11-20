9a02a6a86003cb8a9279bba4baa2a991538e8d33
huseyinozerr <41543642+huseyinozerr@users.noreply.github.com>
2025-11-20 13:54:22 +0300

    Create load_bronze procedure for data loading
    This script creates a stored procedure for loading data into various tables from CSV files, including error handling.

---
+++ scripts/insert_data.sql
@@ -0,0 +1,100 @@
+-- This script creates a procedure called load_bronze. This procedure can be used to truncate our tables and bulk insert data from given CSV files 
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
+BEGIN
+	BEGIN TRY
+		PRINT '================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '================================';
+
+		PRINT '--------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '--------------------------------';
+
+		PRINT '>> Truncating Table: bronze.crm_cust_info'
+		TRUNCATE TABLE bronze.crm_cust_info;
+
+		PRINT '>> Inserting Data Into: bronze.crm_cust_info'
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Program Files\SQL\dwh_project\datasets\source_crm\cust_info.csv'
+
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		PRINT '>> Truncating Table: bronze.crm_prd_info'
+		TRUNCATE TABLE bronze.crm_prd_info;
+
+		PRINT '>> Inserting Data Into: bronze.crm_prd_info'
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Program Files\SQL\dwh_project\datasets\source_crm\prd_info.csv'
+
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		PRINT '>> Truncating Table: bronze.crm_sales_details'
+		TRUNCATE TABLE bronze.crm_sales_details;
+
+		PRINT '>> Inserting Data Into: bronze.crm_sales_details'
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Program Files\SQL\dwh_project\datasets\source_crm\sales_details.csv'
+
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+
+		PRINT '--------------------------------';
+		PRINT 'Loading ERP Tables';
+		PRINT '--------------------------------';
+
+		PRINT '>> Truncating Table: bronze.erp_cust_az12'
+		TRUNCATE TABLE bronze.erp_cust_az12;
+
+		PRINT '>> Inserting Data Into: bronze.erp_cust_az12'
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Program Files\SQL\dwh_project\datasets\source_erp\cust_az12.csv'
+
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		PRINT '>> Truncating Table: bronze.erp_loc_a101'
+		TRUNCATE TABLE bronze.erp_loc_a101;
+
+		PRINT '>> Inserting Data Into: bronze.erp_loc_a101'
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Program Files\SQL\dwh_project\datasets\source_erp\loc_a101.csv'
+
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2'
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2'
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Program Files\SQL\dwh_project\datasets\source_erp\px_cat_g1v2.csv'
+
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+	END TRY 
+	BEGIN CATCH 
+	PRINT '=================================';
+	PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+	PRINT 'Error Message:'+ ERROR_MESSAGE();
+	PRINT 'Error Number:'+ CAST(ERROR_NUMBER() AS NVARCHAR);
+	PRINT 'Error State:'+ CAST(ERROR_STATE() AS NVARCHAR);
+	PRINT '=================================';
+	END CATCH
+END
